6d5db3a3a83cdba26026ee3aa10de6b5ed6e84d6
h602618 <h602618@stud.hvl.no>
2023-01-27 10:50:31 +0100

    Implementerte Message og MessageUtils

--- src/main/java/no/hvl/dat110/messaging/Message.java
+++ src/main/java/no/hvl/dat110/messaging/Message.java
@@ -4,22 +4,18 @@ import no.hvl.dat110.TODO;
 
 public class Message {
 
-	// the up to 127 bytes of data (payload) that a message can hold
-	private byte[] data;
+    // the up to 127 bytes of data (payload) that a message can hold
+    private byte[] data;
 
-	// construction a Message with the data provided
-	public Message(byte[] data) {
-		
-		// TODO - START
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.constructor("Message"));
-			
-		// TODO - END
-	}
+    // construction a Message with the data provided
+    public Message(byte[] data) {
+        if (data != null && data.length < MessageUtils.SEGMENTSIZE) {
+            this.data = data;
+        }
+    }
 
-	public byte[] getData() {
-		return this.data; 
-	}
+    public byte[] getData() {
+        return this.data;
+    }
 
 }
--- src/main/java/no/hvl/dat110/messaging/MessageUtils.java
+++ src/main/java/no/hvl/dat110/messaging/MessageUtils.java
@@ -6,43 +6,37 @@ import no.hvl.dat110.TODO;
 
 public class MessageUtils {
 
-	public static final int SEGMENTSIZE = 128;
+    public static final int SEGMENTSIZE = 128;
 
-	public static int MESSAGINGPORT = 8080;
-	public static String MESSAGINGHOST = "localhost";
+    public static int MESSAGINGPORT = 8080;
+    public static String MESSAGINGHOST = "localhost";
 
-	public static byte[] encapsulate(Message message) {
-		
-		byte[] segment = null;
-		byte[] data;
-		
-		// TODO - START
-		
-		// encapulate/encode the payload data of the message and form a segment
-		// according to the segment format for the messaging layer
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-			
-		// TODO - END
-		return segment;
-		
-	}
+    public static byte[] encapsulate(Message message) {
 
-	public static Message decapsulate(byte[] segment) {
+        byte[] segment = new byte[SEGMENTSIZE];
+        byte[] data = message.getData();
 
-		Message message = null;
-		
-		// TODO - START
-		// decapsulate segment and put received payload data into a message
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - END
-		
-		return message;
-		
-	}
-	
+        // encapulate/encode the payload data of the message and form a segment
+        // according to the segment format for the messaging layer
+
+        segment[0] = (byte) data.length;
+        for (int i = 0; i < data.length; i++) {
+            segment[i + 1] = data[i];
+        }
+
+        return segment;
+
+    }
+
+    public static Message decapsulate(byte[] segment) {
+        // decapsulate segment and put received payload data into a message
+
+        byte[] data = new byte[segment[0]];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = segment[i + 1];
+        }
+
+        return new Message(data);
+    }
+
 }
